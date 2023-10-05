5274718a30f837168b1d982c3578a44875f6bf55
livinha1234 <liviamariasouza268@gmail.com>
2023-10-05 11:47:17 -0300

    Initial commit

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,29 @@
+import styles from './Cardapio.module.scss';
+import logo from 'assets/logo.png';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+
+export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+    return (
+    <main>
+        <br></br>
+        <nav className={styles.menu}>
+            <img src= {logo} alt="logo" />
+        </nav>
+        <header className={styles.header}> 
+            <div className={styles.header__text}>
+            </div>
+        </header>
+        <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+                <Filtros/>
+            </div>
+            </section>
+            
+   </main>
+    );
+}
